0f203a593cde60cbd64191c831bd28fc75676fe4
abc <abc@example.com>
2020-01-18 23:38:12 +0200

    First commit

---
+++ Assets/main_menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class main_menu : MonoBehaviour
+{
+    public void StartApp()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitApp()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
